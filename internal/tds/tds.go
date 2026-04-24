7586a4634009cb31564f04214961518a94aade91
Leonardo Medeiros <105822034+lesoname@users.noreply.github.com>
2026-04-24 15:46:33 -0300

    fix: adiciona modo tds-cli no advpls.exe

--- internal/tds/tds.go
+++ internal/tds/tds.go
@@ -84,6 +84,7 @@ func (c *Client) Delete(filePath string) error {
 
 func (c *Client) runTDSCli(action, filePath string, recompile bool) error {
 	args := []string{
+		"tds-cli",
 		action,
 		"serverType=AdvPL",
 		fmt.Sprintf("server=%s", c.address),
